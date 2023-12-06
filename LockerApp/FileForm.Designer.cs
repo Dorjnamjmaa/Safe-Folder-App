0ae5ccd1b9c38c602b56128f8dc0dbf629f00820
Dorjnamjmaa <124252193+Dorjnamjmaa@users.noreply.github.com>
2023-12-06 16:39:50 +0800

    Create FileForm.Designer.cs

---
+++ LockerApp/FileForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LockerApp
+{
+    partial class FileForm
+    {
+        private System.Windows.Forms.Label filePathLabel;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button lockButton;
+        private System.Windows.Forms.Button unlockButton;
+
+        private void InitializeComponent()
+        {
+            this.filePathLabel = new System.Windows.Forms.Label();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.lockButton = new System.Windows.Forms.Button();
+            this.unlockButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // filePathLabel
+            // 
+            this.filePathLabel.AutoSize = true;
+            this.filePathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filePathLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.filePathLabel.Location = new System.Drawing.Point(156, 145);
+            this.filePathLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.filePathLabel.Name = "filePathLabel";
+            this.filePathLabel.Size = new System.Drawing.Size(120, 22);
+            this.filePathLabel.TabIndex = 7;
+            this.filePathLabel.Text = "Файлын зам:";
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(131)))), ((int)(((byte)(158)))));
+            this.filePathTextBox.Location = new System.Drawing.Point(284, 147);
+            this.filePathTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(363, 22);
+            this.filePathTextBox.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(668, 147);
+            this.browseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(100, 28);
+            this.browseButton.TabIndex = 9;
+            this.browseButton.Text = "Сонгох";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.passwordLabel.Location = new System.Drawing.Point(156, 188);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 22);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Нууц үг:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(131)))), ((int)(((byte)(158)))));
+            this.passwordTextBox.Location = new System.Drawing.Point(284, 190);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(363, 22);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // lockButton
+            // 
+            this.lockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lockButton.Location = new System.Drawing.Point(327, 255);
+            this.lockButton.Margin = new System.Windows.Forms.Padding(4);
+            this.lockButton.Name = "lockButton";
+            this.lockButton.Size = new System.Drawing.Size(121, 50);
+            this.lockButton.TabIndex = 12;
+            this.lockButton.Text = "Файлыг түгжих";
+            this.lockButton.UseVisualStyleBackColor = true;
+            this.lockButton.Click += new System.EventHandler(this.lockButton_Click);
+            // 
+            // unlockButton
+            // 
+            this.unlockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unlockButton.Location = new System.Drawing.Point(479, 255);
+            this.unlockButton.Margin = new System.Windows.Forms.Padding(4);
+            this.unlockButton.Name = "unlockButton";
+            this.unlockButton.Size = new System.Drawing.Size(114, 50);
+            this.unlockButton.TabIndex = 13;
+            this.unlockButton.Text = "Файлыг нээх";
+            this.unlockButton.UseVisualStyleBackColor = true;
+            this.unlockButton.Click += new System.EventHandler(this.unlockButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(332, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 32);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Файл нууцлалт";
+            // 
+            // FileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(35)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(838, 354);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unlockButton);
+            this.Controls.Add(this.lockButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.filePathLabel);
+            this.Name = "FileForm";
+            this.Text = "FileForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
