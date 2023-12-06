e5011f53dc5cc0ab62006089d3c8c7df6c8bfe9a
Dorjnamjmaa <124252193+Dorjnamjmaa@users.noreply.github.com>
2023-12-06 16:41:37 +0800

    Create Program.cs

---
+++ LockerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LockerApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StartForm()); // Start with a login form
+        }
+    }
+}
