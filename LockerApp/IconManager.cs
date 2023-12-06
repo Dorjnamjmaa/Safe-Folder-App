330b663d1308cc6b33268f9cc46989d09f451f5f
Dorjnamjmaa <124252193+Dorjnamjmaa@users.noreply.github.com>
2023-12-06 16:40:26 +0800

    Create IconManager.cs

---
+++ LockerApp/IconManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LockerApp
+{
+    public class IconManager
+    {
+        private Icon lockerIcon;
+        private Icon defaultIcon;
+
+        public IconManager(string lockerIconPath)
+        {
+            lockerIcon = LoadIconFromFile(lockerIconPath);
+        }
+
+        private Icon LoadIconFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return new Icon(filePath);
+            }
+            else
+            {
+                throw new FileNotFoundException($"Icon file not found at path: {filePath}");
+            }
+        }
+
+        public void SetLockerIcon(Form form)
+        {
+            defaultIcon = form.Icon;
+            form.Icon = lockerIcon;
+        }
+
+        public void SetDefaultIcon(Form form)
+        {
+            form.Icon = defaultIcon;
+        }
+    }
+}
