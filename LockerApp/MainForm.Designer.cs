6db1007078a93e3382eda1dcf3242b178ed6f5d4
Dorjnamjmaa <124252193+Dorjnamjmaa@users.noreply.github.com>
2023-12-06 16:40:46 +0800

    Create MainForm.Designer.cs

---
+++ LockerApp/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LockerApp
+{
+    partial class MainForm
+    {
+        private System.Windows.Forms.Label folderPathLabel;
+        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button lockButton;
+        private System.Windows.Forms.Button unlockButton;
+
+        private void InitializeComponent()
+        {
+            this.folderPathLabel = new System.Windows.Forms.Label();
+            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.lockButton = new System.Windows.Forms.Button();
+            this.unlockButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderPathLabel
+            // 
+            this.folderPathLabel.AutoSize = true;
+            this.folderPathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.folderPathLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.folderPathLabel.Location = new System.Drawing.Point(159, 159);
+            this.folderPathLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.folderPathLabel.Name = "folderPathLabel";
+            this.folderPathLabel.Size = new System.Drawing.Size(148, 22);
+            this.folderPathLabel.TabIndex = 0;
+            this.folderPathLabel.Text = "Фолдерийн зам:";
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(131)))), ((int)(((byte)(158)))));
+            this.folderPathTextBox.Location = new System.Drawing.Point(315, 159);
+            this.folderPathTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.Size = new System.Drawing.Size(363, 22);
+            this.folderPathTextBox.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(697, 156);
+            this.browseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(100, 28);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Сонгох";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.passwordLabel.Location = new System.Drawing.Point(159, 198);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 22);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Нууц үг:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(131)))), ((int)(((byte)(158)))));
+            this.passwordTextBox.Location = new System.Drawing.Point(315, 198);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(363, 22);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // lockButton
+            // 
+            this.lockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lockButton.Location = new System.Drawing.Point(361, 253);
+            this.lockButton.Margin = new System.Windows.Forms.Padding(4);
+            this.lockButton.Name = "lockButton";
+            this.lockButton.Size = new System.Drawing.Size(121, 50);
+            this.lockButton.TabIndex = 5;
+            this.lockButton.Text = "Фолдерийг түгжих";
+            this.lockButton.UseVisualStyleBackColor = true;
+            this.lockButton.Click += new System.EventHandler(this.lockButton_Click);
+            // 
+            // unlockButton
+            // 
+            this.unlockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unlockButton.Location = new System.Drawing.Point(505, 253);
+            this.unlockButton.Margin = new System.Windows.Forms.Padding(4);
+            this.unlockButton.Name = "unlockButton";
+            this.unlockButton.Size = new System.Drawing.Size(114, 50);
+            this.unlockButton.TabIndex = 6;
+            this.unlockButton.Text = "Фолдерийг нээх";
+            this.unlockButton.UseVisualStyleBackColor = true;
+            this.unlockButton.Click += new System.EventHandler(this.unlockButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(368, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Фолдер нууцлалт";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(35)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(946, 390);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unlockButton);
+            this.Controls.Add(this.lockButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.folderPathTextBox);
+            this.Controls.Add(this.folderPathLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Folder Locker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
